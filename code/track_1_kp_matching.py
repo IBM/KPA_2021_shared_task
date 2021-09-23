2f5e2d5ad5a387fe731da2f00b423ce1d845c5d2
lenadankin <37078666+lenadankin@users.noreply.github.com>
2021-09-23 22:09:47 +0300

    Update track_1_kp_matching.py
    fix mAP calculation to consider the maximum possible recall instead of the maximum recall in the top 50% predictions.

--- code/track_1_kp_matching.py
+++ code/track_1_kp_matching.py
@@ -12,7 +12,12 @@ def get_ap(df, label_column, top_percentile=0.5):
     df = df.sort_values('score', ascending=False).head(top)
     # after selecting top percentile candidates, we set the score for the dummy kp to 1, to prevent it from increasing the precision.
     df.loc[df['key_point_id'] == "dummy_id", 'score'] = 0.99
-    return average_precision_score(y_true=df[label_column], y_score=df["score"])
+    ap = average_precision_score(y_true=df[label_column], y_score=df["score"])
+    # multiply by the number of positives in top 50% and devide by the number of max positives within the top 50%, which is the number of top 50% instances
+    positives_in_top_predictions = sum(df[label_column])
+    max_num_of_positives = len(df)
+    ap_retrieval = ap * positives_in_top_predictions/max_num_of_positives
+    return ap_retrieval
 
 def calc_mean_average_precision(df, label_column):
     precisions = [get_ap(group, label_column) for _, group in df.groupby(["topic", "stance"])]
